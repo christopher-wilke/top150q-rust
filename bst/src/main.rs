bb9ec732e07126d88801b26fc99e67c6b397e499
Christopher <christopher.wilke86@googlemail.com>
2025-01-15 14:16:47 +0100

    iterative traversal bst

--- bst/src/main.rs
+++ bst/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[derive(Debug)]
 struct Tree {
     root: Option<Box<Node>>
@@ -25,6 +27,38 @@ impl Tree {
         Self {
             root: None
         }
+    }
+
+    fn traverse_level(&self) -> Vec<i32> {
+        if self.root.is_none() {
+            return Vec::new();
+        }
+
+        let mut results: Vec<i32> = Vec::new();
+        let mut q: VecDeque<&Box<Node>> = VecDeque::new();
+        let root = self.root.as_ref().unwrap();
+        results.push(root.value);
+        q.push_back(root);
+
+        let mut height = 0;
+        while !q.is_empty() {
+            for i in 0..q.len() {
+                if let Some(node) = q.pop_front() {
+                    if let Some(left) = &node.left {
+                        results.push(left.value);
+                        q.push_back(left);
+                    }
+                    if let Some(right) = &node.right {
+                        results.push(right.value);
+                        q.push_back(right);
+                    }
+                }
+            }
+            height += 1;
+        }
+        
+        println!("height={height}");
+        results
     }
 
     fn insert(&mut self, value: i32) {
@@ -96,8 +130,22 @@ impl From<Node> for Option<Box<Node>> {
 mod tests {
     use super::*;
     
+    // #[test]
+    // fn works_builds_tree() {
+    //     let mut tree = Tree::new();
+    //     tree.insert(8);
+    //     tree.insert(10);
+    //     tree.insert(3);
+    //     tree.insert(1);
+    //     tree.insert(6);
+    //     tree.insert(4);
+    
+    //     assert_eq!(tree.root.is_some(), true);
+    //     println!("{:?}", tree.traverse_level());
+    // }
+    
     #[test]
-    fn works_builds_tree() {
+    fn works_builds_tree_traversal() {
         let mut tree = Tree::new();
         tree.insert(8);
         tree.insert(10);
@@ -105,9 +153,12 @@ mod tests {
         tree.insert(1);
         tree.insert(6);
         tree.insert(4);
-    
+        tree.insert(7);
+        tree.insert(14);
+        tree.insert(13);
+
         assert_eq!(tree.root.is_some(), true);
-        println!("{:?}", tree);
+        println!("{:?}", tree.traverse_level());
     }
 
 }
